68b4518f3c70f16474a14877d48bea802eb36259
Alex Boldov <chipoid.ru@gmail.com>
2020-10-21 03:35:37 +0300

    выполнено HW_1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,50 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private final Resume[] storage = new Resume[10000];
+    private int resumeCounter;
 
     void clear() {
+        Arrays.fill(storage, 0, resumeCounter, null);
+        resumeCounter = 0;
     }
 
     void save(Resume r) {
+        storage[resumeCounter] = r;
+        resumeCounter++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < resumeCounter; i++) {
+            if (storage[i].uuid.equals(uuid)) return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < resumeCounter; i++) {
+            if (storage[i].uuid.equals(uuid) && resumeCounter + 1 - i >= 0) {
+                System.arraycopy(storage, i + 1, storage, i, resumeCounter + 1 - i);
+                resumeCounter--;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
+        if (resumeCounter != 0) {
+            return Arrays.copyOf(storage, resumeCounter);
+        }
         return new Resume[0];
     }
 
     int size() {
-        return 0;
+        return resumeCounter;
     }
 }
